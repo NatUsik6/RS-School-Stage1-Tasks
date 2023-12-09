f4e53bb6268baff81572c9d68838238cc5d24908
NatUsik6 <tashausik6@gmail.com>
2023-12-09 23:28:09 +0300

    feat: create slider

--- coffee-house/scripts/slider.js
+++ coffee-house/scripts/slider.js
@@ -5,22 +5,30 @@ const lastPosition = 2;
 
 const sliderRightArrowButton = document.querySelector(".favorite-coffee__slider__container__right-arrow-button");
 sliderRightArrowButton.addEventListener("click", moveSliderLeft);
+const sliderLeftArrowButton = document.querySelector(".favorite-coffee__slider__container__left-arrow-button");
+sliderLeftArrowButton.addEventListener("click", moveSliderRight);
+
 const sliderWrapper = document.querySelector(".slider-wrapper");
+const slider = document.querySelector(".slider");
+
+const sliderProgress1 = document.querySelector(".favorite-coffee__slider__controls_page1 .slider__progress");
+const sliderProgress2 = document.querySelector(".favorite-coffee__slider__controls_page2 .slider__progress");
+const sliderProgress3 = document.querySelector(".favorite-coffee__slider__controls_page3 .slider__progress");
+const sliderProgressList = [sliderProgress1, sliderProgress2, sliderProgress3];
+
+sliderProgressList.forEach(sliderProgress => sliderProgress.addEventListener("animationend", moveSliderLeft))
+startSliderProgress();
 
 function moveSliderLeft() {
     increasePosition();
+    setSliderPosition();
+    startSliderProgress();
+}
 
-    switch (currentPosition) {
-        case 0:
-            sliderWrapper.style.justifyContent = "flex-start";
-            break;
-        case 1:
-            sliderWrapper.style.justifyContent = "center";
-            break;
-        case 2:
-            sliderWrapper.style.justifyContent = "flex-end";
-            break;
-    }
+function moveSliderRight() {
+    decreasePosition();
+    setSliderPosition();
+    startSliderProgress();
 }
 
 function increasePosition() {
@@ -29,4 +37,24 @@ function increasePosition() {
 
 function decreasePosition() {
     currentPosition = currentPosition > 0 ? currentPosition - 1 : lastPosition;
+}
+
+function setSliderPosition() {
+    slider.style.transform = `translateX(-${sliderWrapper.offsetWidth * currentPosition}px)`;
+}
+
+function startSliderProgress() {
+    sliderProgressList.forEach(sliderProgress => sliderProgress.classList.remove("active"));
+
+    switch (currentPosition) {
+        case 0:
+            sliderProgress1.classList.add("active");
+            break;
+        case 1:
+            sliderProgress2.classList.add("active");
+            break;
+        case 2:
+            sliderProgress3.classList.add("active");
+            break;
+    }
 }
