d0e419ff9b6b5bbb806f59f57dc1996bd25875b9
NatUsik6 <tashausik6@gmail.com>
2024-01-31 23:17:27 +0300

    feat: add logic to fill horizontal clues

--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,28 +1,53 @@
 "use strict";
 
-import { createElement } from "./common";
+import { createElement } from "./common.js";
 
 const tableWidth = 5;
 const tableHeight = 5;
 
-const main = createElement("main", "main_22", document.querySelector("body"));
+const main = createElement("main", "main", document.querySelector("body"));
 
 const createTable = () => {
   const table = createElement("table", "table", main);
   const firstRow = createElement("tr", "table-row", table);
 
   for (let index = 0; index < tableWidth + 1; index++) {
-    createElement("th", "clue-cell", firstRow);
+    createElement("th", "clue-cell clue-cell-vertical", firstRow);
   }
 
   for (let rowIndex = 0; rowIndex < tableHeight; rowIndex++) {
     const row = createElement("tr", "table-row", table);
-    createElement("th", "clue-cell", row);
+    createElement("th", "clue-cell clue-cell-horizontal", row);
 
     for (let cellIndex = 0; cellIndex < tableWidth; cellIndex++) {
       createElement("td", "cell", row);
     }
   }
 };
+
+const fillClues = async () => {
+  const response = await fetch("../resources/nonograms.json");
+  const nonograms = await response.json();
+  const nonogram = nonograms[0];
+  const horizontalClues = [];
+
+  for (let rowIndex = 0; rowIndex < nonogram.data.length; rowIndex++) {
+    const row = nonogram.data[rowIndex];
+    horizontalClues.push([0]);
+    const clueRow = horizontalClues[rowIndex];
+    
+    for (let cellIndex = 0; cellIndex < row.length; cellIndex++) {
+      const cell = row[cellIndex];
+      
+      if (cell === 0 && clueRow[clueRow.length - 1] !== 0) {
+        clueRow.push(0);
+      }
+
+      if (cell === 1) {
+        clueRow[clueRow.length - 1]++;
+      }
+    }
+  }
+}
 
 createTable();
